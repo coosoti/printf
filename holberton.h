cfb564f67fd7111b2a009dda8cf3ff271730615d
coosoti <charlesosoti@gmail.com>
2021-03-16 12:10:15 +0300

    created struct that matches specifiers with appropriate functions to print

--- holberton.h
+++ holberton.h
@@ -2,27 +2,29 @@
 #define HOLBERTON_H
 
 #include <stdarg.h>
+#include <stdlib.h>
 
 /**
- * struct print - struct to print various formats
- * @type: type to print
+ * struct print - struct containing chrs for comparison with specfiers and
+ * selecting appropriate function
+ * @type: char to compare
  * @funct: function to print
  *
  */
 
 typedef struct print
 {
-	char *type;
+	char type;
 	int (*funct)(va_list);
 
 } print_f;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int printNaN(char c1, char c2);
 
 int printchar(va_list a_list);
 int printstr(va_list a_list);
-void printd(int num);
 int printint(va_list a_list);
-int countint(int num);
+
 #endif
